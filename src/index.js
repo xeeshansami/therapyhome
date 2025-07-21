7ffd1af0ec0fef22c344d5e067da47ea628c0e03
Mohammad.Zeeshan <info.xeeshan@gmail.com>
2025-07-21 12:52:15 +0500

    update 2107202505

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import './index.css';
 import App from './App';
 import store from './redux/store';
 import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom'; // Changed import
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter> {/* Changed component */}
-        <App />
-      </HashRouter>
+      <BrowserRouter basename="/therapyhome"> {/* ✅ important */}
+       <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
